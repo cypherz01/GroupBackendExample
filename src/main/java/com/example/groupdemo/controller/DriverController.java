610e2e9a4c7925c0c5d438599f802f377bc22f12
cypherz01 <mrtomcuthbertson@gmail.com>
2021-10-13 13:40:48 +0000

    controller injected to service layer, mapping for /drivers set up

--- src/main/java/com/example/groupdemo/controller/DriverController.java
+++ src/main/java/com/example/groupdemo/controller/DriverController.java
@@ -1,8 +1,23 @@
 package com.example.groupdemo.controller;
 
+import com.example.groupdemo.model.Driver;
+import com.example.groupdemo.service.DriverService;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class DriverController {
+
+    private final DriverService service;
+
+    public DriverController(DriverService service) {
+        this.service = service;
+    }
+
+    @GetMapping("/drivers")
+    Driver getDriver(@RequestParam("id") Long id){
+        return service.getDriverByID(id);
+    }
 
 }
--- src/main/java/com/example/groupdemo/service/DriverService.java
+++ src/main/java/com/example/groupdemo/service/DriverService.java
@@ -1,4 +1,20 @@
 package com.example.groupdemo.service;
 
+import com.example.groupdemo.model.Driver;
+import com.example.groupdemo.repository.DriverRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
 public class DriverService {
+
+    private final DriverRepository driverRepository;
+
+    public DriverService(DriverRepository driverRepository) {
+        this.driverRepository = driverRepository;
+    }
+
+    public Driver getDriverByID(Long id){
+        return driverRepository.getById(id);
+    }
 }
