1f6f99227a138875d930c45550d5b0de84ebf43e
cypherz01 <mrtomcuthbertson@gmail.com>
2021-10-13 13:02:03 +0000

    first commit

---
+++ src/test/java/com/example/groupdemo/GroupdemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.groupdemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GroupdemoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
